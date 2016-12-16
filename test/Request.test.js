afb5d851bb33af051d01a6e9a11c3e1c2bc62daa
Wilson Júnior <wilsonpjunior@gmail.com>
2016-12-16 16:22:35 -0200

    Add support to repass http method over sandbox

--- test/Request.test.js
+++ test/Request.test.js
@@ -5,6 +5,7 @@ describe('Request', () => {
   let externalRequest;
   before(() => {
     externalRequest = {
+      method: 'PUT',
       headers: {
         host: 'localhost:3000',
         accept: '*/*',
@@ -17,7 +18,11 @@ describe('Request', () => {
   describe('when we need to create new request without data', () => {
     it('should copy data that matches only some parameters of the request', () => {
       const req = new Request(externalRequest);
-      expect(req).to.have.all.keys('headers', 'query', 'body');
+      expect(req).to.have.all.keys('headers', 'query', 'body', 'method');
+      expect(req.method).to.be.eql('PUT');
+      expect(req.query).to.be.eql({ q: 'john' });
+      expect(req.body).to.be.eql({ name: 'John doe' });
+      expect(req.headers).to.be.eql({ host: 'localhost:3000', accept: '*/*' });
     });
   });
 });
