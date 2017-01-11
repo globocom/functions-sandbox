e2e391d7717bacaad14274c724c9aad86b065907
Wilson Júnior <wilsonpjunior@gmail.com>
2017-01-11 19:02:25 -0200

    require: use bind to simplify code complexity

--- test/Require.test.js
+++ test/Require.test.js
@@ -6,9 +6,7 @@ const requireFunction = require;
 describe('Require', () => {
   let sandboxRequire;
 
-  describe('generateRequire', () => {
-    let encapsulatedRequire;
-
+  describe('#require', () => {
     before(() => {
       const modules = {
         './module1': () => ({}),
@@ -19,35 +17,32 @@ describe('Require', () => {
         relativePath: '.',
         globalModules: ['http'],
       });
-      encapsulatedRequire = sandboxRequire.generateRequire();
     });
 
     describe('when we pass a valid module we want to require', () => {
       it('should returns the module insdie myModule is loaded', () => {
-        expect(encapsulatedRequire('./module1')).to.be.eql({});
+        expect(sandboxRequire.require('./module1')).to.be.eql({});
       });
 
       it('should returns an avaliable global module', () => {
-        expect(encapsulatedRequire('http')).to.be.eql(requireFunction('http'));
+        expect(sandboxRequire.require('http')).to.be.eql(requireFunction('http'));
       });
     });
 
     describe('when we pass an invalid module', () => {
       it('should throws an error if invokes a module that does not exist', () => {
-        const fn = () => encapsulatedRequire('does-not-exist');
+        const fn = () => sandboxRequire.require('does-not-exist');
         expect(fn).to.throw(Error, /Cannot find module 'does-not-exist'/);
       });
 
       it('should throws an error when the module exists but is not accessible', () => {
-        const fn = () => encapsulatedRequire('crypto');
+        const fn = () => sandboxRequire.require('crypto');
         expect(fn).to.throw(Error, /Cannot find module 'crypto'/);
       });
     });
   });
 
-  describe('generateRelativeRequire', () => {
-    let relativeRequire;
-
+  describe('#relativeRequire', () => {
     before(() => {
       const modules = {
         './module1': () => ({}),
@@ -59,41 +54,40 @@ describe('Require', () => {
         relativePath: '.',
         globalModules: ['http'],
       });
-      relativeRequire = sandboxRequire.generateRelativeRequire();
     });
 
     describe('when we pass a valid module we want to require', () => {
       it('should be able to call module2 inside mypackage', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         expect(moduleRequire('module2')).to.be.eql({});
       });
 
       it('should be able to call module1 by full path', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         expect(moduleRequire('./module1')).to.be.eql({});
       });
 
       it('should returns an avaliable global module', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         expect(moduleRequire('http')).to.be.eql(requireFunction('http'));
       });
     });
 
     describe('when we pass an invalid module', () => {
       it('should throws an error if invokes a module that does not exist', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         const fn = () => moduleRequire('does-not-exist');
         expect(fn).to.throw(Error, /Cannot find module 'does-not-exist'/);
       });
 
       it('should throws an error if invokes directly a module of another package', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         const fn = () => moduleRequire('module3');
         expect(fn).to.throw(Error, /Cannot find module 'module3'/);
       });
 
       it('should throws an error when the module exists but is not accessible', () => {
-        const moduleRequire = relativeRequire('mypackage2');
+        const moduleRequire = sandboxRequire.relativeRequire('mypackage2');
         const fn = () => moduleRequire('crypto');
         expect(fn).to.throw(Error, /Cannot find module 'crypto'/);
       });
