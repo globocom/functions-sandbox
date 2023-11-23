34a50bfad7e0380b74b627797e0ee80fa36a8279
Celso Inácio <celso.inacio@g.globo>
2023-11-23 10:19:12 -0300

    add fork sandbox feature

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = require('./lib/Sandbox');
+module.exports.executeFunctionInSandbox = require('./lib/ForkSandbox').executeFunctionInSandbox;
 module.exports.PrefixLog = require('./lib/PrefixLog');
 module.exports.MemoryStream = require('./lib/MemoryStream');
